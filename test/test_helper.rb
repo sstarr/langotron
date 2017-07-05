3c8814ae6b0b923496ab79962182f19705215034
Simon Starr <simon@starr.cx>
2017-07-05 21:35:29 +0100

    Add webmock and vcr gems to mock API calls
    So that we can test calls to GitHub's API.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
